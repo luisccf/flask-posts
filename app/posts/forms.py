d21a4be8f34a3e47fe5f05b2cc4f04ce531c462a
luisccf <luisccf@me.com>
2016-11-21 15:31:58 -0200

    Adds wtforms validators to add post form

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from wtforms.widgets import TextArea
 
 class PostForm(Form):
-    text = StringField('text', validators=[DataRequired()], widget=TextArea())
+    text = StringField('text', validators=[DataRequired('You haven\'t typed anything.'), Length(max=140, message='Posts must have 140 characters at max.')], widget=TextArea())
--- app/posts/views.py
+++ app/posts/views.py
@@ -4,7 +4,9 @@ from forms import PostForm
 from flask_login import login_user, logout_user, current_user, login_required
 import datetime
 
+
 mod = Blueprint('posts', __name__, static_folder='./static', template_folder='./templates')
+
 
 @mod.route('/posts/add', methods=['GET', 'POST'])
 @login_required
@@ -13,8 +15,6 @@ def add():
 	if request.method == 'POST':
 		if form.validate_on_submit():
 			response = request.form.to_dict()
-			if len(response['text']) > 140:
-				return render_template('add_post.html', title='Add Post', users=models.User.query.all(), form=form, message='Post too long.')
 			post = models.Post(text=response['text'], timestamp=datetime.datetime.utcnow(), user_id=current_user.id)
 			db.session.add(post)
 			db.session.commit()
@@ -22,9 +22,11 @@ def add():
 		return render_template('add_post.html', title='Add Post', users=models.User.query.all(), form=form, message='You haven\'t typed anything.')
 	return render_template('add_post.html', title='Add Post', header='New post', users=models.User.query.all(), form=form)
 
+
 @mod.route('/posts')
 def posts():
 	return render_template('posts.html', title='All Posts', posts=models.Post.query.all(), user_page=0)
+
 
 @mod.route('/posts/<int:post_id>')
 def post(post_id):
@@ -32,6 +34,7 @@ def post(post_id):
 	if post is None:
 		return render_template('error.html', title='Error', error='No post with id ' + str(post_id))
 	return render_template('post.html', title='Post', post=post)
+
 
 
 @mod.route('/posts/<int:post_id>/remove-<int:user_page>')
@@ -46,6 +49,7 @@ def remove(post_id, user_page):
 	if user_page == 1:
 		return redirect(url_for('users.user', nickname=user.nickname))
 	return redirect(url_for('posts.posts'))
+
 
 @mod.route('/posts/<int:post_id>/comment', methods=['GET', 'POST'])
 @login_required
